b9b145dd014621c20ee00e1a700fcd924dda1abc
Stanslaus Omondi <somondi@microsoft.com>
2023-04-03 20:57:39 +0300

    update transaction type list

--- models/TransactionType.js
+++ models/TransactionType.js
@@ -1,8 +1,13 @@
 const TransactionType = Object.freeze({
   Annual: "Annual ",
   Monthly: "Monthly",
+  Registration: "Registration",
   Welfare: "Welfare",
+  Fine: "Fine",
+  Medical: "Medical",
   GetTogether: "Get Together",
+  FamilyRound: "Family Round",
+  Dividends: "Dividends",
 });
 
 module.exports = {
