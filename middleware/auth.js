0f55b1585926690103eebb47e960944fc381d80f
Stanslaus Omondi <somondi@microsoft.com>
2023-04-10 15:25:38 +0300

    update token workflow

--- middleware/auth.js
+++ middleware/auth.js
@@ -21,7 +21,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
   // Make sure token exists
   if (!token) {
-    return next(new ErrorResponse("Not authorized to access this route", 401));
+    return next(new ErrorResponse("Not authorized to access this route-no token", 401));
   }
 
   try {
@@ -30,7 +30,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.user = await User.findById(decoded.id);
     next();
   } catch (err) {
-    return next(new ErrorResponse("Not authorized to access this route", 401));
+    return next(new ErrorResponse("Not authorized to access this route - token not verified", 401));
   }
 });
 
